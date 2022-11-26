6f779dcb5e84f50ac29dafac04487e120352fa9d
Yuta Sawamura <>
2022-11-26 22:16:46 +0900

    Update:

--- server/src/SuperHero.java
+++ server/src/SuperHero.java
@@ -2,4 +2,9 @@ public class SuperHero extends Hero {
     public SuperHero(String name) {
         super(name);
     }
+
+    @Override
+    public int attack() {
+        return new java.util.Random().nextInt(4) + 1;
+    }
 }
